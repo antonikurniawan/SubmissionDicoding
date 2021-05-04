d5a890c8b6cf46352ad4f9d06ab5ae28f14568ad
anton <antonikrnwn@gmail.com>
2021-05-04 15:10:03 +0700

    this is jetpack sub two

---
+++ app/src/main/java/com/dicoding/picodiploma/jetpacksubmissiondua/data/source/remote/response/TvResponse.kt
@@ -0,0 +1,14 @@
+package com.dicoding.picodiploma.jetpacksubmissiondua.data.source.remote.response
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class TvResponse(
+		var id: String,
+		var title: String,
+		var overview: String,
+		var poster_path: String,
+): Parcelable
